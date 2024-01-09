db24d362aa55f01351226311d37ee02772291b03
ishanjarwal <ishan.professional01@gmail.com>
2024-01-09 14:11:48 +0530

    email working

--- validator.js
+++ validator.js
@@ -1,10 +1,9 @@
 const { check } = require('express-validator');
 
-const formvalidate = [
+exports.formvalidate = [
     check('fullname').not().isEmpty().withMessage("Please provide your name"),
     check('interest').not().isEmpty().withMessage("Please select one"),
     check('contact').not().isEmpty().withMessage("Please provide your contact details"),
     check('projectBrief').not().isEmpty().withMessage("Please describe your project ideas"),
     check('budget').not().isEmpty().withMessage("Please select one")
 ]
-export default formvalidate;
